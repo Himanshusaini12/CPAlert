c7183c53632646d98e603d033bf2db535f356dc8
Hardy04 <107628694+Hardy04@users.noreply.github.com>
2022-06-17 21:35:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "./header";
 import Footer from "./footer";
-
+import Body from "./body";
 function App() {
   return (
     <div>
       <Header />
+      <Body />
       <Footer />
     </div>
   );
