71eb6da4b67ba7fb1e3c51c789a5991d68e5fd7b
Himanshusaini12 <107533833+Himanshusaini12@users.noreply.github.com>
2022-06-26 08:16:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,48 @@ fetch('https://codeforces.com/api/contest.list')
 
 		document.getElementById('codeforces').innerHTML = displaydata;
 })
+
+//---------------------------codechef-------------------------------
+
+
+fetch('https://kontests.net/api/v1/code_chef')
+.then((alldata) => {
+	return alldata.json();
+})
+.then((data) => {
+		var displaydata = "";
+
+		console.log(data)
+
+        data.map((values)=>{
+			{
+			displaydata+= `<p>Name : ${values.name} <br/> 
+			start_time : ${values.start_time} hours
+			`;
+		}
+		});
+
+		document.getElementById('codechef').innerHTML = displaydata;
+})
+
+//-----------------------------------leetcode------------------------------
+
+fetch('https://kontests.net/api/v1/leet_code')
+.then((alldata) => {
+	return alldata.json();
+})
+.then((data) => {
+		var displaydata = "";
+
+		console.log(data)
+
+        data.map((values)=>{
+			{
+			displaydata+= `<p>Name : ${values.name} <br/> 
+			start_time : ${values.start_time} hours
+			`;
+		}
+		});
+
+		document.getElementById('leetcode').innerHTML = displaydata;
+})
